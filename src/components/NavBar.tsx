dce52c9f08628886617517757dcf802bd61e4043
Joe <jyarnott@gmail.com>
2025-01-17 15:27:15 -0600

    Added a color mode button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
+
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} alt="logo" boxSize="60px" />
       <Text>Nav Bar</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
