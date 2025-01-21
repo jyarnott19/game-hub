33f27f885db43abaf7ab077fc59f3a066cef601b
Joe <jyarnott@gmail.com>
2025-01-21 13:44:43 -0600

    Show a spinner while fetching genres. Null if error.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,28 +1,32 @@
 import useGenres from "@/hooks/useGenres";
 import getUrlWithInsertedText from "@/services/image-url";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, loading, error } = useGenres();
   return (
-    <List.Root variant="plain">
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getUrlWithInsertedText(
-                genre.image_background,
-                "media/",
-                "crop/600/400/"
-              )}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List.Root>
+    <>
+      {loading && <Spinner />}
+      {error && null}
+      <List.Root variant="plain">
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getUrlWithInsertedText(
+                  genre.image_background,
+                  "media/",
+                  "crop/600/400/"
+                )}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
